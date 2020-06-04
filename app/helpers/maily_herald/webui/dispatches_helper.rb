c31b1ccef51a3a351f6c2da2c7d8db8aa4bb7847
Stayman <stayman.hou@gmail.com>
2020-06-04 14:05:10 -0400

    update ui for new static start time one-time mailings lifecycle

--- app/helpers/maily_herald/webui/dispatches_helper.rb
+++ app/helpers/maily_herald/webui/dispatches_helper.rb
@@ -2,10 +2,26 @@ module MailyHerald
   module Webui
     module DispatchesHelper
       def display_dispatch_state s
-        content_tag(:span) do 
+        content_tag(:span) do
           case s.state
           when :enabled, :pending, :completed
             boolean_icon true, style: :toggle, text: :enabled
+          when :disabled
+            boolean_icon false, style: :toggle, text: :enabled
+          when :archived
+            icon("archive", tw("commons.archived"))
+          end
+        end
+      end
+      def display_dispatch_state_extra_detailed s
+        content_tag(:span) do
+          case s.state
+          when :enabled
+            boolean_icon true, style: :toggle, text: :enabled
+          when :pending
+            icon("clock-o", tw("commons.pending"))
+          when :completed
+            icon("check-square-o", tw("commons.completed"))
           when :disabled
             boolean_icon false, style: :toggle, text: :enabled
           when :archived
