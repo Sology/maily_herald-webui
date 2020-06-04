bc5a0c5bde8738431e5a8c26f26ac9481325614c
Stayman <stayman.hou@gmail.com>
2020-06-04 10:39:34 -0400

    move ad-hoc send all action to background

--- app/helpers/maily_herald/webui/dispatches_helper.rb
+++ app/helpers/maily_herald/webui/dispatches_helper.rb
@@ -4,7 +4,7 @@ module MailyHerald
       def display_dispatch_state s
         content_tag(:span) do 
           case s.state
-          when :enabled
+          when :enabled, :pending, :completed
             boolean_icon true, style: :toggle, text: :enabled
           when :disabled
             boolean_icon false, style: :toggle, text: :enabled
